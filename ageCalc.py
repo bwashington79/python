13fba54829c6ceafffc291a1e034fc46ff0b47a5
Bobby-Washington <bobwashington@hotmail.com>
2024-09-24 11:26:03 -0400

    Update ageCalc.py

--- ageCalc.py
+++ ageCalc.py
@@ -1,14 +1,14 @@
 import datetime as date
 currentYear = date.datetime.today().strftime('%Y')
 
+'''
+get current year
 print(currentYear)
+'''
 
 print("This program will calculate your age in decades.")
 name: str = input("Please provide your name\n")
-age: int = input("Please provide your curret age\n")
-decades: int = age/100
-print(decades)
-
-
-
-
+age = int(input("Please provide your current age\n"))
+decades: int = age // 10
+years: int = age % 10
+print("Hello " + name + " you are " + str(decades) + " decades AND " + str(years) + " years old!")
