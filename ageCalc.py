8fc810696f31471a2d47969dbd92a4ad8ab82448
Bobby-Washington <bobwashington@hotmail.com>
2024-10-01 10:20:10 -0400

    Learning conditionals

--- ageCalc.py
+++ ageCalc.py
@@ -6,9 +6,24 @@ get current year
 print(currentYear)
 '''
 
+# using conditionals
+
+
 print("This program will calculate your age in decades.")
 name: str = input("Please provide your name\n")
 age = int(input("Please provide your current age\n"))
 decades: int = age // 10
 years: int = age % 10
-print("Hello " + name + " you are " + str(decades) + " decades AND " + str(years) + " years old!")
+print("Hello " + name + " you are " + str(decades) +
+      " decades AND " + str(years) + " years old!")
+
+temp = int(input("What's the current temp where you are located?\n"))
+
+if temp < 65:
+    print(("It's cold!! put on a coat"))
+elif temp > 65 or temp < 69:
+      print(("You may want to consider a jacket"))
+
+else:
+    print("Enjoy the outdoors!")
+
