01f638ca39c23dddcf14f37d6686e80752cbc046
Bobby-Washington <bobwashington@hotmail.com>
2024-10-15 17:11:05 -0400

    Learning Higher Order Functions

---
+++ pluralsight/functions_and_decorators.py
@@ -0,0 +1,19 @@
+# Learning functions and decerators
+
+'''
+def add_one(number):
+    return number + 1
+
+
+print('The value of the add_one() function is', add_one(2))
+
+'''
+
+def say_hello(name):
+    return f"Hello {name}"
+
+def be_awesome(name):
+    return f"Yo {name}, together we're the awesomeest!"
+
+def greet_bob(greeter_func):
+    return greeter_func("Bob")
---
+++ pluralsight/higher_order_functions.py
@@ -0,0 +1,13 @@
+# Make two utilities functions
+
+def add(num1, num2):
+    return num1 + num2
+
+def subtract(num1 - num2):
+    return num1 - num2
+
+
+# Passing in a function into another function
+
+def function_plus_one(func, num1, num2):
+    return func(num1, num2) + 1
